dbaecb3861c125d0e86a770e62e0a96c5914777c
Georgiy <olenchenko.h.m@nmu.one>
2025-03-06 17:21:47 +0200

    feat: added new API endpoints for main page categories

--- src/main/java/com/olenchenko/controllers/backend/ApiController.java
+++ src/main/java/com/olenchenko/controllers/backend/ApiController.java
@@ -20,7 +20,33 @@ public class ApiController {
     @GetMapping(value = "/api/newproducts", produces = "application/json")
     public String getMainPage() {
         Gson gson = new Gson();
-        String res = gson.toJson(touchParser.getNewProducts());
-        return res;
+        return gson.toJson(touchParser.getNewProducts());
     }
+    @GetMapping(value = "/api/mergedcategories", produces = "application/json")
+    public String getMergedCategoriesFromMainPage() {
+        Gson gson = new Gson();
+        return gson.toJson(touchParser.getMergedCategories());
+    }
+    @GetMapping(value = "/api/sales", produces = "application/json")
+    public String getSales() {
+        Gson gson = new Gson();
+        return gson.toJson(touchParser.getSales());
+    }
+    @GetMapping(value = "/api/markdown", produces = "application/json")
+    public String getMarkdown() {
+        Gson gson = new Gson();
+        return gson.toJson(touchParser.getMarkdown());
+    }
+    @GetMapping(value = "/api/bestsellers", produces = "application/json")
+    public String getBestSellers() {
+        Gson gson = new Gson();
+        return gson.toJson(touchParser.getBestSellers());
+    }
+    @GetMapping(value = "/api/refreshdata", produces = "application/json")
+    public String refreshData() {
+        touchParser.refreshMainPage();
+        Gson gson = new Gson();
+        return gson.toJson("Data refreshed");
+    }
+
 }
