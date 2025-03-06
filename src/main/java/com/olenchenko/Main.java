94731f287b85578d55a00e0d2ae31ae446838aff
Georgiy <olenchenko.h.m@nmu.one>
2025-03-06 15:46:52 +0200

    feat: updating project to SpringBoot project

--- src/main/java/com/olenchenko/Main.java
+++ src/main/java/com/olenchenko/Main.java
@@ -1,12 +1,11 @@
 package com.olenchenko;
 
-import com.google.gson.Gson;
-import com.olenchenko.parser.TouchParser;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        TouchParser touchParser = new TouchParser();
-        Gson gson = new Gson();
-        System.out.println(gson.toJson(touchParser.getNewProducts()));
+        SpringApplication.run(Main.class, args);
     }
 }
