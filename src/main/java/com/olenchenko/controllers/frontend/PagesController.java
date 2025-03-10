ef78054f14ac94106d54f838ccbd5cec84c6c624
Georgiy <olenchenko.h.m@nmu.one>
2025-03-10 19:32:22 +0200

    feat: Using lombok for some variables. "Transferred HashMaps to ProductCard and Product Models. Implemented refreshing data.

--- src/main/java/com/olenchenko/controllers/frontend/PagesController.java
+++ src/main/java/com/olenchenko/controllers/frontend/PagesController.java
@@ -1,5 +1,8 @@
 package com.olenchenko.controllers.frontend;
 
+import com.olenchenko.Model.ProductCard;
+import com.olenchenko.parser.TouchParser;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -7,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
@@ -17,23 +21,29 @@ import static com.olenchenko.Constants.*;
 @Controller
 public class PagesController {
     private final String apiUrl;
+    @Autowired
     public PagesController(@Value("${api}") String apiUrl) {
         this.apiUrl = apiUrl;
     }
     // The given ParameterizedTypeReference is used to pass generic type information.
     // https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/web/client/RestTemplate.html#exchange(java.net.URI,org.springframework.http.HttpMethod,org.springframework.http.HttpEntity,org.springframework.core.ParameterizedTypeReference)
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @RequestParam(required = false, defaultValue = "false") boolean refresh) {
+        if (refresh) {
+            RestTemplate restTemplate = new RestTemplate();
+            restTemplate.getForObject(apiUrl + "refreshdata", String.class);
+        }
+
         String url = apiUrl + "mergedcategories";
         RestTemplate restTemplate = new RestTemplate();
-        ResponseEntity<HashMap<String, List<HashMap<String, Object>>>> response = restTemplate.exchange(
+        ResponseEntity<HashMap<String, List<ProductCard>>> response = restTemplate.exchange(
                 url,
                 HttpMethod.GET,
                 null,
                 new ParameterizedTypeReference<>() {
                 }
         );
-        HashMap<String, List<HashMap<String, Object>>> products = response.getBody();
+        HashMap<String, List<ProductCard>> products = response.getBody();
         products.forEach((key, value) -> {
             switch (key) {
                 case newProductsText -> model.addAttribute("newproducts", value);
