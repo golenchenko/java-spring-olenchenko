1a4507589178ef8b969503f6b88cc2bc49dd03bf
Georgiy <olenchenko.h.m@nmu.one>
2025-03-11 18:20:46 +0200

    refactor: refactoring code

--- src/main/java/com/olenchenko/controllers/backend/ApiController.java
+++ src/main/java/com/olenchenko/controllers/backend/ApiController.java
@@ -7,7 +7,6 @@ import com.olenchenko.services.ExcelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
